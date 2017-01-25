71da2e994656038ff6c6b7df298ded23d1ee6570
Dmitry on work <karasev.dmitry@gmail.com>
2017-01-25 18:28:38 +0400

    code review, переделал вкладки на странице Оборудование

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-$(function () {
+jQuery(function ($) {
     var modalWindows = $("div[role='dialog']"); //модальные окна
-    var controlButtons = $("*[role='button'][aria-controls]"); //кнопки управления модальными окнами
-
+    var controlButtons = $("*[role='button'][aria-controls]"); //control-buttons управления модальными окнами
+    var controlRows = $("tr.office-row");
     //задаем параметры модальных окон
     modalWindows.each(function () {
         var result = $(this).dialog(
@@ -21,7 +21,8 @@ $(function () {
 
     //задаем обработчик событий на кнопки
     controlButtons.each(function () {
-        $(this).on("click", function () {
+        $(this).on("click", function (event) {
+            event.stopPropagation()
             var window = {
                 id: $(this).attr("aria-controls"),
                 dataId: $(this).data("id")
@@ -41,159 +42,38 @@ $(function () {
         })
     });
 
-    $( "#menu" ).menu();
+    //задаем обработчик событий на клики по строкам таблицы
+    controlRows.each(function () {
+        $(this).on("click", function (event) {
+            console.log(this);
 
-// Добавление статуса
-//     var addStatus = $("#addStatus-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addStatus-btnOpen").on("click", function () {
-//         addStatus.dialog("open");
-//     });
-//     $("#addStatus-btnClose").on("click", function () {
-//         addStatus.dialog("close");
-//     });
-//
-// // Добавление региона
-//     var addRegion = $("#addRegion-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addRegion-btnOpen").on("click", function () {
-//         addRegion.dialog("open");
-//     });
-//     $("#addRegion-btnClose").on("click", function () {
-//         addRegion.dialog("close");
-//     });
-//
-// // Добавление города
-//     var addCity = $("#addCity-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addCity-btnOpen").on("click", function () {
-//         addCity.dialog("open");
-//     });
-//     $("#addCity-btnClose").on("click", function () {
-//         addCity.dialog("close");
-//     });
-//
-// // Добавление офиса
-//     var addOffice = $("#addOffice-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addOffice-btnOpen").on("click", function () {
-//         addOffice.dialog("open");
-//     });
-//     $("#addOffice-btnClose").on("click", function () {
-//         addOffice.dialog("close");
-//     });
-
-
-//JQuery
-    var deviceTab = $('#hw-sw-dict-tabs');
-    var vendorTab = $('#hw-sw-dict-tabs #tab-vendors');
-    var platformTab = $('#hw-sw-dict-tabs #tab-platforms');
-    deviceTab.tabs();
-    vendorTab.click(function() {
-        deviceTab.removeClass(function (index, className) {
-            var clName = className.match(/(^|\s)col-(xs|sm|md|lg)-\d+/g).join(' ');
-            console.log(clName);
-            return clName;
-        }).addClass('col-xs-7');
-    });
-    platformTab.click(function() {
-        deviceTab.removeClass(function (index, className) {
-            var clName = className.match(/(^|\s)col-(xs|sm|md|lg)-\d+/g).join(' ');
-            console.log(clName);
-            return clName;
-        }).addClass('col-xs-7');
+            var window = {
+                id: $(this).attr("aria-controls"),
+                dataId: $(this).data("id")
+            };
+            modalWindows.filter("[id=" + window.id + "]").dialog("open");
+            // console.log($(this).attr("value"));
+            // console.log(window.dataId);
+        })
     });
 
-// // Добавление вендора
-//     var addVendor = $("#addVendor-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addVendor-btnOpen").on("click", function () {
-//         addVendor.dialog("open");
-//     })
-//     $("#addVendor-btnClose").on("click", function () {
-//         addVendor.dialog("close");
-//     })
-// // Редактирование вендора
-//     var editVendor = $("#editVendor-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#editVendor-btnOpen").on("click", function () {
-//         editVendor.dialog("open");
-//     })
-//     $("#editVendor-btnClose").on("click", function () {
-//         editVendor.dialog("close");
-//     })
-//
-//
-// // Добавление платформы
-//     var addPlatform = $("#addPlatform-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addPlatform-btnOpen").on("click", function () {
-//         addPlatform.dialog("open");
-//     })
-//     $("#addPlatform-btnClose").on("click", function () {
-//         addPlatform.dialog("close");
-//     })
-//
-// // Добавление ПО платформы
-//     var addSoftware = $("#addSoftware-modal").dialog({
-//         autoOpen: false,
-//         height: "auto",
-//         width: "auto",
-//         modal: true
-//     });
-//
-//     $("#addSoftware-btnOpen").on("click", function () {
-//         addSoftware.dialog("open");
-//     })
-//     $("#addSoftware-btnClose").on("click", function () {
-//         addSoftware.dialog("close");
-//     })
-// Детальная инфа по офису
-    var officeDetail = $("#officeDetails-modal").dialog({
-        autoOpen: false,
-        height: "auto",
-        width: "auto",
-        modal: true
-    })
-    $(".office-row").on("click", function() {
-        console.log($(this).data("id"));
-        officeDetail.dialog("open");
-    })
-})
+//Tabs
+    var tabs = $(".nav-tabs[role='tablist'] li[role='tab']");
+    var panels = $(".tab-content div[role=tabpanel]");
+    console.log(tabs);
+
+    //задаем обработчик событий на клики по табам
+    tabs.each(function() {
+        $(this).on( "click", function () {
+            tabs.filter("[aria-selected='true']").removeClass("active").attr('aria-selected','false');
+            $(this).addClass("active").attr('aria-selected','true');
+
+            panels.filter(".active").removeClass("active");
+            var panelId = $(this).attr("aria-controls");
+            panels.filter("[id=" + panelId + "]").addClass("active");
+            // console.log("[id=" + panelId + "]");
+        })
+    });
+});
 
 
--- public/js/testscript.js
+++ public/js/testscript.js
@@ -1,13 +1,13 @@
 jQuery(function ($) {
-    var tabs = $(".nav-tabs li[role='tab']");
+    var tabs = $(".nav-tabs[role='tablist'] li[role='tab']");
     var panels = $(".tab-content div[role=tabpanel]");
 
     tabs.each(function() {
         $(this).on( "click", function (event) {
-            tabs.filter(".active").removeClass("active");
-            $(event.target).parent().addClass("active");
+            tabs.filter("[aria-selected='true']").removeClass("active").attr('aria-selected','false');
+            $(this).addClass("active").attr('aria-selected','true');
             panels.filter(".active").removeClass("active");
-            var panelId = $(event.target).parent().attr("aria-controls");
+            var panelId = $(this).attr("aria-controls");
             console.log("[id=" + panelId + "]");
             panels.filter("[id=" + panelId + "]").addClass("active");
         })
