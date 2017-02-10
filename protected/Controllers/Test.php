d5b9fe4a237ed18b8dadfdae150a920b6a270778
Dmitry on work <karasev.dmitry@gmail.com>
2017-02-10 17:41:56 +0400

    В тестовом контроллере сделал сортировку офисов по регионам

--- protected/Controllers/Test.php
+++ protected/Controllers/Test.php
@@ -5,6 +5,8 @@ namespace App\Controllers;
 
 use App\Models\Address;
 use App\Models\City;
+use App\Models\Office;
+use App\Models\OfficeStatus;
 use App\Models\Region;
 use T4\Core\IArrayable;
 use T4\Mvc\Controller;
@@ -37,4 +39,21 @@ class Test extends Controller
 
         die;
     }
+
+    /**
+     * action вывода всех офисов
+     */
+    public function actionOffices()
+    {
+        $asc = function (Office $office_1, Office $office_2) {
+            return strnatcmp($office_1->address->city->region->title, $office_2->address->city->region->title);
+        };
+        $all = Office::findAll();
+        $sorted = $all->uasort($asc);
+
+        $this->data->offices = $sorted;
+        $this->data->regions = Region::findAll();
+        $this->data->statuses = OfficeStatus::findAll();
+    }
+
 }
