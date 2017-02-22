9e3575dade750469e5388ed5c0c65523c38467cb
Dmitry on work <karasev.dmitry@gmail.com>
2017-02-22 15:00:33 +0400

    переделка скрипта табов

--- protected/Layouts/resources/js/script.js
+++ protected/Layouts/resources/js/script.js
@@ -1,4 +1,5 @@
 jQuery(function ($) {
+
     var body = $('body');
     //console.log($("[role='button'][data-action]"));
     body.on(
@@ -16,7 +17,6 @@ jQuery(function ($) {
                         dataType: "html"
                     })
                         .done(function (html) {
-                            //console.log(html);
                             var modalWindow = $(html);
 
                             modalWindow.dialog({
@@ -48,75 +48,6 @@ jQuery(function ($) {
         }
     );
 
-
-
-
-    // var modalWindows = $("div[role='dialog']"); //модальные окна
-    //
-    // //задаем параметры модальных окон
-    // modalWindows.each(function () {
-    //     var winHeight = $(window).height();
-    //     var winWidth = $(window).width();
-    //     var result = $(this).dialog(
-    //         {
-    //             open: function(){
-    //                 $("body").css("overflow", "hidden");
-    //             },
-    //             close: function(){
-    //                 $("body").css("overflow", "auto");
-    //             },
-    //             position: { my: "center top", at: "top+5%", of: window },
-    //             autoOpen: false,
-    //             height: "auto",
-    //             maxHeight: winHeight * 0.9,
-    //             width: "auto",
-    //             modal: true
-    //         }
-    //     );
-    // });
-
-    //если надо поменять параметры окна
-    // modalWindows.filter("[id='add-region']").dialog(
-    //     {width: 400}
-    // );
-
-    //задаем обработчик событий на кнопки
-    // controlButtons.each(function () {
-    //     $(this).on("click", function (event) {
-    //         //event.stopPropagation()
-    //         var window = {
-    //             id: $(this).attr("aria-controls"),
-    //             dataId: $(this).data("id")
-    //         };
-    //         var action = $(this).attr("value");
-    //
-    //         switch(action) {
-    //             case 'open':
-    //                 modalWindows.filter("[id=" + window.id + "]").dialog("open");
-    //                 break;
-    //             case 'close':
-    //                 modalWindows.filter("[id=" + window.id + "]").dialog("close");
-    //                 break;
-    //         }
-    //         // console.log($(this).attr("value"));
-    //         // console.log(window.dataId);
-    //     })
-    // });
-
-    //задаем обработчик событий на клики по строкам таблицы
-    // controlRows.each(function () {
-    //     $(this).on("click", function (event) {
-    //         var window = {
-    //             id: $(this).attr("aria-controls"),
-    //             dataId: $(this).data("id")
-    //         };
-    //         modalWindows.filter("[id=" + window.id + "]").dialog("open");
-    //         // console.log($(this).attr("value"));
-    //         // console.log(window.dataId);
-    //         //переключаем фокус на элемент у которого [aria-selected='true']
-    //         tabs.filter("[aria-selected='true']").children().get(0).focus();
-    //     })
-    // });
 //Tabs
     var tabs = $(".nav-tabs[role='tablist'] li[role='tab']");
     var panels = $(".tab-content div[role=tabpanel]");
