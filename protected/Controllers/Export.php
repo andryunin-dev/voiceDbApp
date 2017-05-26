eb77622c8a4ebf17b35b62661e291a6bcc1b43e6
Andrew Kucherenko <k2.andri@gmail.com>
2017-05-26 13:57:46 +0400

    Add the controller for export hard inventory. Add the export button

--- protected/Controllers/Export.php
+++ protected/Controllers/Export.php
@@ -6,122 +6,17 @@ use App\Models\Appliance;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use T4\Mvc\Controller;
 
 class Export extends Controller
 {
-    public function actionExcellll()
+    public function actionHardInvExcel()
     {
-        $spreadsheet = new Spreadsheet();
-
-        // 4.6.21.	Setting the default style of a workbook
-//        $spreadsheet->getDefaultStyle()->getFont()->setName('Arial')->setSize(10);
-
-         /// 4.6.28.	Setting a column’s width
-        $spreadsheet->getActiveSheet()->getColumnDimension('A:L')->setAutoSize(true);
-        // Default
-//        $spreadsheet->getActiveSheet()->getDefaultColumnDimension()->setWidth(12);
-
-        // 4.6.31.	Setting a row’s height
-//        $spreadsheet->getActiveSheet()->getRowDimension('10')->setRowHeight(100);
-//        $spreadsheet->getActiveSheet()->getDefaultRowDimension()->setRowHeight(15);
-
-        // 4.6.34.	Merge/unmerge cells
-//        $spreadsheet->getActiveSheet()->mergeCells('A18:E22');
-//        $spreadsheet->getActiveSheet()->unmergeCells('A18:E22');
-
-        // 4.6.38.	Add rich text to a cell
-//        $objRichText = new PHPExcel_RichText();
-//        $objRichText->createText('This invoice is ');
-//
-//        $objPayable = $objRichText->createTextRun('payable within thirty days after the end of the month');
-//        $objPayable->getFont()->setBold(true);
-//        $objPayable->getFont()->setItalic(true);
-//        $objPayable->getFont()->setColor( new PHPExcel_Style_Color( PHPExcel_Style_Color::COLOR_DARKGREEN ) );
-//
-//        $objRichText->createText(', unless specified otherwise on the invoice.');
-//
-//        $spreadsheet->getActiveSheet()->getCell('A18')->setValue($objRichText);
-
-        // 4.6.18.	Formatting cells
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getFont()->getColor()->setARGB(PHPExcel_Style_Color::COLOR_RED);
-//
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
-//        $spreadsheet->getActiveSheet()->getStyle('A1:D4')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_TOP);
-//
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
-//
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
-//        $spreadsheet->getActiveSheet()->getStyle('B2')->getFill()->getStartColor()->setARGB('FFFF0000');
-//        // cell range as a parameter
-//        $spreadsheet->getActiveSheet()->getStyle('B3:B7')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setARGB('FFFF0000');
-
-
-
-        // Add header
-        $spreadsheet->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'Регион')
-            ->setCellValue('B1', 'Офис')
-            ->setCellValue('C1', 'Hostname')
-            ->setCellValue('D1', 'Type')
-            ->setCellValue('E1', 'Device')
-            ->setCellValue('F1', 'Device ser.')
-            ->setCellValue('G1', 'Software')
-            ->setCellValue('H1', 'Software ver.')
-            ->setCellValue('I1', 'Module')
-            ->setCellValue('J1', 'Module ser.')
-            ->setCellValue('K1', 'Comment')
-            ->setCellValue('L1', 'In use');
-
-//        $appliances = Appliance::findAll();
-        $appliance = Appliance::findByPK('18096');
-//        var_dump($appliance->modules->first());die;
-
-        $n = 2;
-        foreach ($appliance->modules as $module) {
-            $spreadsheet->setActiveSheetIndex(0)
-                ->setCellValue('A' . $n, $appliance->location->address->city->region->title)
-                ->setCellValue('B' . $n, $appliance->location->title)
-                ->setCellValue('C' . $n, $appliance->details->hostname)
-                ->setCellValue('D' . $n, $appliance->type)
-                ->setCellValue('E' . $n, $appliance->platform->platform->vendor->title . ' ' .$appliance->platform->platform->title)
-                ->setCellValue('F' . $n, $appliance->platform->serialNumber)
-
-                ->setCellValue('G' . $n, $module->module->title)
-                ->setCellValue('H' . $n, $module->serialNumber)
-
-                ->setCellValue('I' . $n, $appliance->software->software->title)
-                ->setCellValue('J' . $n, $appliance->software->version)
-
-                ->setCellValue('K' . $n, $module->comment)
-                ->setCellValue('L' . $n, $module->inUse);
-
-            $n++;
-        }
-
-    }
-
-    /**
-     *
-     */
-    public function actionExcel()
-    {
-        // Create new Spreadsheet object
         $spreadsheet = new Spreadsheet();
 
         // HEADER
-        $spreadsheet->getActiveSheet()->getStyle('A1:M1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-        $spreadsheet->getActiveSheet()->getStyle('A1:M1')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
-
-        $columns = ['A','B','C','D','E','F','G','H','I','J','K','L','M'];
-        foreach ($columns as $column) {
-            $spreadsheet->getActiveSheet()->getColumnDimension($column)->setAutoSize(true);
-        }
-
-        // Add some data
         $spreadsheet->setActiveSheetIndex(0)
             ->setCellValue('A1', '№п/п')
             ->setCellValue('B1', 'Регион')
@@ -129,13 +24,56 @@ class Export extends Controller
             ->setCellValue('D1', 'Hostname')
             ->setCellValue('E1', 'Type')
             ->setCellValue('F1', 'Device')
-            ->setCellValue('G1', 'Device ser.')
+            ->setCellValue('G1', 'Device ser')
             ->setCellValue('H1', 'Software')
             ->setCellValue('I1', 'Software ver.')
-            ->setCellValue('J1', 'Module')
-            ->setCellValue('K1', 'Module ser.')
-            ->setCellValue('L1', 'Comment')
-            ->setCellValue('M1', 'In use');
+            ->setCellValue('J1', 'Appl. last update')
+            ->setCellValue('K1', 'Module')
+            ->setCellValue('L1', 'Module ser.')
+            ->setCellValue('M1', 'Module last update')
+            ->setCellValue('N1', 'Comment');
+
+        $appliances = Appliance::findAll();
+
+        // Format
+        $columns = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N'];
+        foreach ($columns as $column) {
+            $spreadsheet->getActiveSheet()->getColumnDimension($column)->setAutoSize(true);
+        }
+        $spreadsheet->getActiveSheet()->getStyle('A:N')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+        $spreadsheet->getActiveSheet()->getStyle('A1:N1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+        // Body
+        $n = 2;
+        foreach ($appliances as $appliance) {
+            foreach ($appliance->modules as $module) {
+                $spreadsheet->setActiveSheetIndex(0)
+                    ->setCellValue('A' . $n, $n-1)
+                    ->setCellValue('B' . $n, $appliance->location->address->city->region->title)
+                    ->setCellValue('C' . $n, $appliance->location->title)
+                    ->setCellValue('D' . $n, $appliance->details->hostname)
+                    ->setCellValue('E' . $n, $appliance->type)
+                    ->setCellValue('F' . $n, $appliance->platform->platform->vendor->title . ' ' .$appliance->platform->platform->title)
+                    ->setCellValue('G' . $n, $appliance->platform->serialNumber)
+                    ->setCellValue('H' . $n, $appliance->software->software->title)
+                    ->setCellValue('I' . $n, $appliance->software->version)
+                    ->setCellValue('J' . $n, (new \DateTime($appliance->lastUpdate))->format('d-m-Y'))
+                    ->setCellValue('K' . $n, $module->module->title)
+                    ->setCellValue('L' . $n, $module->serialNumber)
+                    ->setCellValue('M' . $n, (new \DateTime($module->lastUpdate))->format('d-m-Y'))
+                    ->setCellValue('N' . $n, $module->comment)
+                    ->getStyle('A' . $n . ':N' . $n)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+
+                if (false === $module->inUse && false === $module->notFound) {
+                    $spreadsheet->getActiveSheet()->getStyle('K' . $n . ':M' . $n)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_YELLOW);
+                }
+                if (false === $module->inUse && true === $module->notFound) {
+                    $spreadsheet->getActiveSheet()->getStyle('K' . $n . ':M' . $n)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_RED);
+                }
+
+                $n++;
+            }
+        }
 
         // Rename worksheet
         $spreadsheet->getActiveSheet()->setTitle('Appliances');
@@ -145,7 +83,7 @@ class Export extends Controller
 
         // Redirect output to a client’s web browser (Excel2007)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="Appliances '. gmdate('d M Y') . '.xlsx"');
+        header('Content-Disposition: attachment;filename="Hard inventory__'. gmdate('d M Y') . '.xlsx"');
         header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
