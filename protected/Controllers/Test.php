d97df0c9d2a11d94581bcc5b9de04883eacdf01f
Karasev Dmitry at Work <karasev.dmitry@gmail.com>
2017-09-13 18:31:55 +0400

    regular

--- protected/Controllers/Test.php
+++ protected/Controllers/Test.php
@@ -2,64 +2,14 @@
 
 namespace App\Controllers;
 
-
-use App\Components\ContentFilter;
-use App\Components\Paginator;
-use App\Components\Sorter;
-use App\Components\TableFilter;
-use App\Models\LotusLocation;
-use App\ViewModels\DevModulePortGeo;
-use T4\Core\Std;
-use T4\Core\Url;
-use T4\Dbal\Query;
 use T4\Mvc\Controller;
 
 class Test extends Controller
 {
     public function actionDefault()
     {
-        $hrefFilter = [
-            'href' => null,
-            'reg' => [
-                'eq' => 'Hreg_1',
-                'like' => 'Hreg_2'
-            ],
-            'city' => [
-                'eq' => 'Hcity 1, Hcity2, Hcity 3',
-                'like' => 'Hcity like'
-            ],
-            'soft' => [
-                'eq' => 123,
-                'like' => 'Hsoft like'
-            ]
-        ];
-        $tabFilter = [
-            'reg' => [
-                'eq' => 'Treg_1',
-                'like' => 'Treg_2'
-            ],
-            'city' => [
-                'eq' => 'Tcity 1, Tcity2, Tcity 3',
-                'like' => 'Tcity like'
-            ],
-            'soft' => [
-                'eq' => 123,
-                'like' => 'Tsoft like'
-            ]
-        ];
-        $sort = [
-            'col1',
-            'col2',
-            'col3',
-        ];
-        var_dump(new Paginator());die;
     }
     public function actionTwigTest()
     {
-//        LotusLocation::countPeoples();
-        $query = (new Query())
-            ->select()
-            ->from(DevModulePortGeo::getTableName());
-        $res = DevModulePortGeo::officeIdListByQuery($query, 'lotusId');
     }
 }
