7a502aa21b5bba12009237be7016192bf70fa769
Andrew Kucherenko <k2.andri@gmail.com>
2018-07-05 11:19:09 +0400

    make a copy for PhoneStatsByCallsReport

---
+++ Configs/devGeoPivotStatisticByCallsWithBodyFooter.php
@@ -0,0 +1,397 @@
+<?php
+
+return [
+  'dataUrl' => '/report/PhoneStatsByCallsReportHandler.json',
+  'connection' => '',
+  'className' => 'App\\ViewModels\\DevGeo_ViewMat',
+  'columns' =>
+  [
+    'region' =>
+    [
+      'id' => 'region',
+      'name' => 'Регион',
+      'width' => 10,
+      'sortable' => true,
+      'filterable' => true,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'city' =>
+    [
+      'id' => 'city',
+      'name' => 'Город',
+      'width' => 10,
+      'sortable' => true,
+      'filterable' => true,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'office' =>
+    [
+      'id' => 'office',
+      'name' => 'Офис',
+      'width' => 15,
+      'sortable' => true,
+      'filterable' => true,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'lotus_employees' =>
+    [
+      'id' => 'people-v',
+      'name' => 'Сотрудников',
+      'width' => '60px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'phoneAmount' =>
+    [
+      'id' => 'phone-count',
+      'name' => 'кол-во тел.',
+      'width' => '60px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'HWActive' =>
+    [
+      'id' => 'hw-active-v',
+      'name' => 'HW Phones<br>(актив.)',
+      'width' => '60px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+        0 => 'class_1',
+        1 => 'class_2',
+      ],
+    ],
+    'HWNotActive' =>
+    [
+      'id' => 'hw-not-active-v',
+      'name' => 'HW Phones<br>(не актив.)',
+      'width' => '60px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+        0 => 'class_1',
+        1 => 'class_2',
+      ],
+    ],
+    'notHWActive' =>
+    [
+      'id' => 'not-hw-active-v',
+      'name' => 'virtual & analog<br>Phones(актив.)',
+      'width' => '60px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+        0 => 'class_1',
+        1 => 'class_2',
+      ],
+    ],
+    'plTitle' =>
+    [
+      'id' => 'pl',
+      'name' => 'Оборудование',
+      'width' => 65,
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => true,
+      'classes' =>
+      [
+      ],
+    ],
+    'plTitleActive' =>
+    [
+      'id' => 'pl_active',
+      'name' => '',
+      'width' => 0,
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => false,
+      'classes' =>
+      [
+      ],
+    ],
+    'lotusId' =>
+    [
+      'id' => 'lot_id',
+      'name' => 'ID',
+      'width' => '50px',
+      'sortable' => false,
+      'filterable' => false,
+      'visible' => false,
+      'classes' =>
+      [
+      ],
+    ],
+  ],
+  'calculated' =>
+  [
+    'phoneAmount' =>
+    [
+      'column' => 'appliance_id',
+      'method' => 'count',
+      'preFilter' =>
+      [
+      ],
+      'selectBy' =>
+      [
+      ],
+    ],
+    'HWActive' =>
+    [
+      'column' => 'appType',
+      'method' => 'count',
+      'preFilter' =>
+      [
+        'appType' =>
+        [
+          'eq' =>
+          [
+            0 => 'phone',
+          ],
+        ],
+        'isHW' =>
+        [
+          'eq' =>
+          [
+            0 => 'true',
+          ],
+        ],
+        'appAge' =>
+        [
+          'lt' =>
+          [
+            0 => 73,
+          ],
+        ],
+      ],
+      'selectBy' =>
+      [
+        0 => 'lotusId',
+      ],
+    ],
+    'HWNotActive' =>
+    [
+      'column' => 'appType',
+      'method' => 'count',
+      'preFilter' =>
+      [
+        'appType' =>
+        [
+          'eq' =>
+          [
+            0 => 'phone',
+          ],
+        ],
+        'isHW' =>
+        [
+          'eq' =>
+          [
+            0 => 'true',
+          ],
+        ],
+        'appAge' =>
+        [
+          'ge' =>
+          [
+            0 => 73,
+          ],
+        ],
+      ],
+      'selectBy' =>
+      [
+        0 => 'lotusId',
+      ],
+    ],
+    'notHWActive' =>
+    [
+      'column' => 'appType',
+      'method' => 'count',
+      'preFilter' =>
+      [
+        'appType' =>
+        [
+          'eq' =>
+          [
+            0 => 'phone',
+          ],
+        ],
+        'isHW' =>
+        [
+          'eq' =>
+          [
+            0 => 'false',
+          ],
+        ],
+        'appAge' =>
+        [
+          'lt' =>
+          [
+            0 => 73,
+          ],
+        ],
+      ],
+      'selectBy' =>
+      [
+        0 => 'lotusId',
+      ],
+    ],
+  ],
+  'aliases' =>
+  [
+  ],
+  'extraColumns' =>
+  [
+  ],
+  'bodyFooterTable' => 'devGeoPivotStatisticByCallsWithBodyFooterBF',
+  'sortOrderSets' =>
+  [
+    'region' =>
+    [
+      'region' => '',
+      'city' => '',
+      'office' => '',
+    ],
+    'city' =>
+    [
+      'city' => '',
+      'office' => '',
+    ],
+  ],
+  'sortBy' =>
+  [
+    'region' => '',
+    'city' => '',
+    'office' => '',
+  ],
+  'preFilter' =>
+  [
+    'appType' =>
+    [
+      'eq' =>
+      [
+        0 => 'phone',
+      ],
+    ],
+  ],
+  'pagination' =>
+  [
+    'rowsOnPageList' =>
+    [
+      0 => 10,
+      1 => 50,
+      2 => 100,
+      3 => 200,
+      4 => 500,
+      5 => 'все',
+    ],
+  ],
+  'cssStyles' =>
+  [
+    'header' =>
+    [
+      'table' =>
+      [
+        0 => 'bg-primary',
+        1 => 'table-bordered',
+        2 => 'table-header-rotated',
+      ],
+    ],
+    'body' =>
+    [
+      'table' =>
+      [
+        0 => 'table',
+        1 => 'cell-bordered',
+        2 => 'cust-table-striped',
+      ],
+    ],
+    'footer' =>
+    [
+      'table' =>
+      [
+      ],
+    ],
+  ],
+  'sizes' =>
+  [
+    'width' => 100,
+    'height' => '',
+  ],
+  'pivot' =>
+  [
+    'plTitle' =>
+    [
+      'column' => 'platformTitle',
+      'preFilter' =>
+      [
+        'appType' =>
+        [
+          'eq' =>
+          [
+            0 => 'phone',
+          ],
+        ],
+      ],
+      'selectBy' =>
+      [
+        0 => 'lotusId',
+      ],
+      'sortBy' =>
+      [
+        'platformTitle' => 'asc',
+      ],
+      'itemWidth' => '67px',
+    ],
+    'plTitleActive' =>
+    [
+      'column' => 'platformTitle',
+      'preFilter' =>
+      [
+        'appType' =>
+        [
+          'eq' =>
+          [
+            0 => 'phone',
+          ],
+        ],
+        'appAge' =>
+        [
+          'lt' =>
+          [
+            0 => 73,
+          ],
+        ],
+      ],
+      'selectBy' =>
+      [
+        0 => 'lotusId',
+      ],
+      'sortBy' =>
+      [
+      ],
+      'itemWidth' => 0,
+    ],
+  ],
+];
