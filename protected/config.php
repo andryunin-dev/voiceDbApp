010b71a288d7928f5b0a006848b3e2b0e7f718a1
Dmitry K Work <karasev.dmitry@gmail.com>
2018-07-30 11:39:40 +0400

    test gitignore

--- protected/config.php
+++ protected/config.php
@@ -25,7 +25,7 @@ return [
         ],
         'cdr' => [
             'driver' => 'pgsql',
-            'host' => '',
+            'host' => 'test',
             'user' => '',
             'password' => '',
             'dbname' => ''
