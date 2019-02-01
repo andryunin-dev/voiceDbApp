db863cd73c941df9fbc2f27abefc08232c6e1394
Karasev DL work <karasev.dmitry@gmail.com>
2019-02-02 00:02:15 +0400

    fix editable tag

---
+++ public/react/editDevModal/precache-manifest.87da89d5b659a08e684217a5303a963e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab7b3880c775d526eba",
+    "url": "/react/editDevModal/static/js/runtime~main.bab7b388.js"
+  },
+  {
+    "revision": "be06b1d406052a56c824",
+    "url": "/react/editDevModal/static/js/main.be06b1d4.chunk.js"
+  },
+  {
+    "revision": "9729162a257c9fcc62b6",
+    "url": "/react/editDevModal/static/js/1.9729162a.chunk.js"
+  },
+  {
+    "revision": "be06b1d406052a56c824",
+    "url": "/react/editDevModal/static/css/main.8b6cd09a.chunk.css"
+  },
+  {
+    "revision": "ca30f93e8bdeeaa049c9e5e8581a7fe6",
+    "url": "/react/editDevModal/index.html"
+  }
+];
--- public/react/editDevModal/service-worker.js
+++ public/react/editDevModal/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/editDevModal/precache-manifest.ee17318fc86e6384241fd8dc726f79c0.js"
+  "/react/editDevModal/precache-manifest.87da89d5b659a08e684217a5303a963e.js"
 );
 
 workbox.clientsClaim();
